2e5f1988571fce5d2d22f68f6dbfccc57e48c7fa
IzzyRahaman <izzyrahaman@gmail.com>
2014-11-22 16:26:44 -0400

    Modified querying for correlating hashtags

--- libs/relationships.js
+++ libs/relationships.js
@@ -13,10 +13,10 @@ function RelationshipManager(db) {
     var q = require("q");
     var mutil = require("mutil");
 
-    var promisifiedQuery = function(query, params) {
+    var promisifiedQuery = function (query, params) {
         var deffered = q.defer();
-        db.query(query, params, function(err, res) {
-            if(err) {
+        db.query(query, params, function (err, res) {
+            if (err) {
                 console.log("Error in executing query");
                 console.log(JSON.stringify(err));
                 deffered.reject(new Error(err));
@@ -29,10 +29,10 @@ function RelationshipManager(db) {
 
     var that = this;
 
-    var createUserHashtagFollowing = function(node_id, hashtag_id) {
+    var createUserHashtagFollowing = function (node_id, hashtag_id) {
         var deffered = q.defer();
-        db.relate(node_id, config.user_to_hashtag_rel_name, hashtag_id, {}, function(err, res) {
-            if(err) {
+        db.relate(node_id, config.user_to_hashtag_rel_name, hashtag_id, {}, function (err, res) {
+            if (err) {
                 deffered.reject();
             } else {
                 deffered.resolve(res);
@@ -41,17 +41,17 @@ function RelationshipManager(db) {
     }
 
     // For GET wrt a user following hastags
-    this.getFollowedHashtags = function(user_identifier, callback) {
+    this.getFollowedHashtags = function (user_identifier, callback) {
         var query = [
             'MATCH (n:User)-[rel:SUBSCRIBES_TO]->(h:Hashtag)',
             'WHERE n.identifier = {user_i}',
             'RETURN h,rel'
         ].join('\n');
         var params = {
-            user_i : user_identifier
+            user_i: user_identifier
         }
         var fn = config.id;
-        if(callback !== undefined) {
+        if (callback !== undefined) {
             console.log("using defined fun");
             fn = callback;
         }
@@ -60,24 +60,24 @@ function RelationshipManager(db) {
     }
 
     // For DELETE wrt a user following a hashtag
-    this.userUnfollowHashtag = function(user_identifier, hashtag_name, callback) {
+    this.userUnfollowHashtag = function (user_identifier, hashtag_name, callback) {
         var query = [
             'MATCH (u:User)-[follows:SUBSCRIBES_TO]-(h:Hashtag)',
             'WHERE u.identifier = {user_i} AND h.name = {hashtag_i}',
             'DELETE follows'
         ].join("\n")
         var param = {
-            user_i : user_identifier,
-            hashtag_i : hashtag_name
+            user_i: user_identifier,
+            hashtag_i: hashtag_name
         };
         var fn = config.id;
-        if(callback) {
+        if (callback) {
             fn = callback;
         }
         return promisifiedQuery(query, param).then(fn).fail(config.error_print("can't delete following")).done();
     }
 
-    this.getCorrelatedHashtags = function(hashtag1, callback) {
+    this.getCorrelatedHashtags = function (hashtag1, callback) {
 
         var query = [
             'MATCH P = (h1:Hashtag)-[rel:CORRELATED_WITH*]->(h2:Hashtag)',
@@ -87,36 +87,82 @@ function RelationshipManager(db) {
         ].join('\n');
 
         var fn = config.id;
-        if(callback) {
+        if (callback) {
             fn = callback;
         }
 
-        return promisifiedQuery(query, {}).then(fn).fail(config.error_print("erroring getting correlated hashtags"));
-
-
-    }
-
-    this.addToCorrelationBetweenHashtags = function(hashtag1, hashtag2, callback) {
-        var query = [
-            'MERGE (h1:Hashtag  {name: "' + hashtag1 +'"})',
-            'ON CREATE SET h1.count=1.0',
-            'ON MATCH SET h1.count = h1.count + 1.0',
-            'MERGE (h2:Hashtag  {name: "' + hashtag2 +'"})',
-            'ON CREATE SET h2.count=1.0',
-            'ON MATCH SET h2.count = h2.count + 1.0',
-            'CREATE UNIQUE (h1)-[rel:CORRELATED_WITH {times: 0.0, cost: 0.0}]->(h2)',
-            'SET rel.times = rel.times + 1.0, rel.cost = 100.0 - (100.0 * (rel.times/h1.count))',
-            'CREATE UNIQUE (h2)-[rel2:CORRELATED_WITH {times: 0.0, cost: 0.0}]->(h1)',
-            'SET rel2.times = rel2.times + 1.0, rel2.cost = 100.0 - (100.0 * (rel2.times/h2.count))'
-        ].join('\n');
-        var fn = config.id;
-        if(callback) {
-            fn = callback;
-        }
         return promisifiedQuery(query, {}).then(fn).
-            fail(config.error_print("Error handling hashtag connections")).done();
+            fail(config.error_print("erroring getting correlated hashtags")).done();
+
 
     }
+
+    var createOrUpdateHashtag = function (hashtag, callback) {
+        console.log("creating hashtag for " + hashtag);
+        var query = [
+            'MERGE (h:Hashtag {name: "' + hashtag + '"})',
+            'ON CREATE SET h.count = 1.0',
+            'ON MATCH SET h.count = h.count + 1.0'
+        ].join('\n');
+        db.query(query, {}, callback);
+        //promisifiedQuery(query, {}).then(callback).fail(config.error_print("Error creating or updating tag")).done();
+    }
+
+    this.addToCorrelationBetweenHashtags = function (hashtag1, hashtag2, callback) {
+
+        createOrUpdateHashtag(hashtag1, function (err, data) {
+            if (err) {
+                console.log("Hashtag " + hashtag1 + " was not created");
+                console.log(new Error(err));
+            } else {
+                console.log("Hashtag " + hashtag1 + " was created");
+                console.log("created for " + JSON.stringify(data));
+                createOrUpdateHashtag(hashtag2, function (err1, data1) {
+                    if (err1) {
+                        console.log(new Error(err1));
+                    } else {
+                        console.log("Hashtag for " + hashtag2 + " was created");
+                        console.log("created for " + JSON.stringify(data1));
+                        var query = [
+                            'MATCH (h1:Hashtag {name:"' + hashtag1 + '"}), (h2:Hashtag {name:"' + hashtag2 + '"})',
+                            'CREATE UNIQUE (h1)-[rel:CORRELATED_WITH {times: 0.0, cost: 0.0}]->(h2)',
+                            'SET rel.times = rel.times + 1.0, rel.cost = 100.0 - (100.0 * (rel.times/h1.count))',
+                            'CREATE UNIQUE (h2)-[rel2:CORRELATED_WITH {times: 0.0, cost: 0.0}]->(h1)',
+                            'SET rel2.times = rel2.times + 1.0, rel2.cost = 100.0 - (100.0 * (rel2.times/h2.count))'
+                        ].join('\n');
+                        promisifiedQuery(query, {}).then(callback).fail(function(err) {
+                            console.log("Error in creating or updating relationship");
+                            console.log(new Error(err));
+                        }).done();
+                    }
+                });
+            }
+
+        });
+    }
+
+
+    /*
+     var query = [
+     'MERGE (h1:Hashtag  {name: "' + hashtag1 +'"})',
+     'ON CREATE SET h1.count=1.0',
+     'ON MATCH SET h1.count = h1.count + 1.0',
+     'MERGE (h2:Hashtag  {name: "' + hashtag2 +'"})',
+     'ON CREATE SET h2.count=1.0',
+     'ON MATCH SET h2.count = h2.count + 1.0',
+     'CREATE UNIQUE (h1)-[rel:CORRELATED_WITH {times: 0.0, cost: 0.0}]->(h2)',
+     'SET rel.times = rel.times + 1.0, rel.cost = 100.0 - (100.0 * (rel.times/h1.count))',
+     'CREATE UNIQUE (h2)-[rel2:CORRELATED_WITH {times: 0.0, cost: 0.0}]->(h1)',
+     'SET rel2.times = rel2.times + 1.0, rel2.cost = 100.0 - (100.0 * (rel2.times/h2.count))'
+     ].join('\n');
+     var fn = config.id;
+     if(callback) {
+     fn = callback;
+     }
+     return promisifiedQuery(query, {}).then(fn).
+     fail(config.error_print("Error handling hashtag connections")).done(); */
+
+
 
     // For POST wrt a user following a hashtag
     this.userFollowHashtag = function(user_identifier, hashtag_name, callback) {
