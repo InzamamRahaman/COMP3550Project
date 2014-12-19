077fb179a889676578740083d03e0e7eed4a2d55
IzzyRahaman <izzyrahaman@gmail.com>
2014-12-18 22:46:47 -0400

    testing charting

--- app/js/angularApps/subgraphController.js
+++ app/js/angularApps/subgraphController.js
@@ -5,9 +5,9 @@
 
     angular.module('app').controller('subgraphController', subgraphController);
 
-    subgraphController.$inject = ['$scope', 'subgraphService']
+    subgraphController.$inject = ['$scope', 'subgraphService', 'socketService'];
 
-    function subgraphController($scope, subgraphService) {
+    function subgraphController($scope, subgraphService, socketService) {
 
         $scope.data = null;
 
@@ -19,7 +19,17 @@
         });
 
         $scope.plot_graph = function() {
+            console.log("Data to plot");
+            console.log($scope.data);
             console.log('Plotting graph');
+            var graphs_to_plot = _.filter($scope.data, function(obj) {
+                return obj.cached && obj.in_use;
+            });
+            var graphs = _.map(graphs_to_plot, 'graph');
+            var usable_data = _.map(graphs, 'data');
+            console.log("Plotting graph for");
+            console.log(usable_data);
+
 
         }
 
